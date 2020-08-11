35c9893f9fc5c75576e4e8efef0a1147e99867ab
bishopfox <h4ng3r@users.noreply.github.com>
2020-08-11 19:40:59 +0200

    First commit with more stuff

---
+++ publish.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+echo "Generating the static blog..."
+
+rm -rf /tmp/hiveminds &> /dev/null
+
+hugo --themesDir ../.. -b https://blog.hiveminds.es -d /tmp/hiveminds
+
+echo "Setting up the GitHub repo and pushing"
+
+cd /tmp/hiveminds && git init && git remote add origin git@github.com:hiveminds-es/hiveminds-es.github.io.git
+
+git add * && git commit -am "Publising `date`" && git push -f origin gh-pages
+
+echo "Blog published to gh-pages"
+
+rm -rf /tmp/hiveminds &> /dev/null
+
+echo "Bye ;)"
